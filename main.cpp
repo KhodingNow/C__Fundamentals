3b86b0dc140a3b10ebd0ff6314f7896b6d3b0365
Thembaletu <khodingtata@gmail.com>
2024-02-16 19:39:50 +0200

    Initial commit, cpp variables

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 34;
+    age = 19;
+    float weight = 65.6;
+    double balance = 1234.567;
+    char gender = 'f';
+    string username = "Pha";
+    bool isregister = false;
+    string colours[10] = {"red", "yellow", "green", "purple"};
+    cout <<colours[1];
+    cin >> colours [0];
+    cout << colours[0];
+
+
+    system("pause>0");
+
+
+}
+
+
+
